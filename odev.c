81ec615dc63754d321d79c79e97267a250c5f080
DESKTOP-EH5IHIH\User <batyrovshatlyk25@gmail.com>
2025-11-21 23:58:07 +0300

    VKI hesaplama ve Ekrana yazdirma yaptik

--- odev.c
+++ odev.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
     char isim[20];
-    float kilo, boy;
+    float kilo, boy, vki;
+    
+    //Kullanicidan veri alma 
     printf("Hosh geldiniz! \nIsminizi giriniz:");
     scanf("%s", &isim);
     printf("Kilonuzu giriniz:");
     scanf("%f", &kilo);
     printf("Boyunuzu giriniz:");
     scanf("%f", &boy);
+    
+    //Vucut Kitle Hesaplamasi 
+    vki = kilo/(pow(boy/100,2));
+    
+    //Ekrana yazdirma
+    printf("\n\nMerhaba %s, kutlen:%.1f, boyun:%.1f, vkin:%.1f\ngule gule", isim, kilo, boy, vki);
     return 0;
 }
