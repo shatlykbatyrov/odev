dcac9781eb1fdc3f7cf2d0a897978489bcfbe180
DESKTOP-EH5IHIH\User <batyrovshatlyk25@gmail.com>
2025-11-21 23:34:56 +0300

    Kullanicidan veri aldik

--- odev.c
+++ odev.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(){
-    
+    char isim[20];
+    float kilo, boy;
+    printf("Hosh geldiniz! \nIsminizi giriniz:");
+    scanf("%s", &isim);
+    printf("Kilonuzu giriniz:");
+    scanf("%f", &kilo);
+    printf("Boyunuzu giriniz:");
+    scanf("%f", &boy);
     return 0;
 }
